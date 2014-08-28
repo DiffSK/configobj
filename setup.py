ad9024e50a9df4a2b8a1a019e07bb133c3df31bf
Daniel Watkins <daniel@daniel-watkins.co.uk>
2014-08-28 18:49:04 +0200

    Switch from using distutils to setuptools.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 # http://opensource.org/licenses/BSD-3-Clause
 import os
 import sys
-from distutils.core import setup
+from setuptools import setup
 # a simple import wouldn't work if we moved towards a package with __init__
 from _version import __version__
 
