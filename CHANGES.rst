a4e29883cab6305e562b67f26e54ab57cc266cf9
jhermann <jh@web.de>
2016-08-26 16:18:44 +0200

    refactored changelog into CHANGES.rst

---
+++ CHANGES.rst
@@ -0,0 +1,28 @@
+Changelog
+---------
+
+Release 5.1.0
+"""""""""""""
+
+* TODO
+
+
+Older Releases
+""""""""""""""
+
+* Release 5.0.6 improves error messages in certain edge cases
+* Release 5.0.5 corrects a unicode-bug that still existed in writing files
+* Release 5.0.4 corrects a unicode-bug that still existed in reading files after
+  fixing lists of string in 5.0.3
+* Release 5.0.3 corrects errors related to the incorrectly handling unicode
+  encoding and writing out files
+* Release 5.0.2 adds a specific error message when trying to install on
+  Python versions older than 2.5
+* Release 5.0.1 fixes a regression with unicode conversion not happening
+  in certain cases PY2
+* Release 5.0.0 updates the supported Python versions to 2.6, 2.7, 3.2, 3.3
+  and is otherwise unchanged
+* Release 4.7.2 fixes several bugs in 4.7.1
+* Release 4.7.1 fixes a bug with the deprecated options keyword in 4.7.0.
+* Release 4.7.0 improves performance adds features for validation and
+  fixes some bugs.
--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@
 
 # This software is licensed under the terms of the BSD license.
 # http://opensource.org/licenses/BSD-3-Clause
+import io
 import os
 import re
 import sys
@@ -49,7 +50,9 @@ assert re.match(r"[0-9](\.[0-9]+)", VERSION), "No semantic version found in 'con
 LONG_DESCRIPTION = """**ConfigObj** is a simple but powerful config file reader and writer: an *ini
 file round tripper*. Its main feature is that it is very easy to use, with a
 straightforward programmer's interface and a simple syntax for config files.
-It has lots of other features though :
+
+List of Features
+----------------
 
 * Nested sections (subsections), to any level
 * List values
@@ -66,23 +69,14 @@ It has lots of other features though :
 * The order of keys/sections is preserved
 * Powerful ``unrepr`` mode for storing/retrieving Python data-types
 
-| Release 5.0.6 improves error messages in certain edge cases
-| Release 5.0.5 corrects a unicode-bug that still existed in writing files
-| Release 5.0.4 corrects a unicode-bug that still existed in reading files after
-| fixing lists of string in 5.0.3
-| Release 5.0.3 corrects errors related to the incorrectly handling unicode
-| encoding and writing out files
-| Release 5.0.2 adds a specific error message when trying to install on
-| Python versions older than 2.5
-| Release 5.0.1 fixes a regression with unicode conversion not happening
-| in certain cases PY2
-| Release 5.0.0 updates the supported Python versions to 2.6, 2.7, 3.2, 3.3
-| and is otherwise unchanged
-| Release 4.7.2 fixes several bugs in 4.7.1
-| Release 4.7.1 fixes a bug with the deprecated options keyword in
-| 4.7.0.
-| Release 4.7.0 improves performance adds features for validation and
-| fixes some bugs."""
+"""
+
+try:
+    with io.open('CHANGES.rst', encoding='utf-8') as handle:
+        LONG_DESCRIPTION += handle.read()
+except EnvironmentError as exc:
+    # Build / install anyway
+    print("WARNING: Cannot open/read CHANGES.rst ({})".format(exc))
 
 CLASSIFIERS = [
     # Details at http://pypi.python.org/pypi?:action=list_classifiers
