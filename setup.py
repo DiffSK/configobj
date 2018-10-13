f33d6829d8eaeda21fb0dcc6fb60996f0f4d7594
Hugo <hugovk@users.noreply.github.com>
2018-10-13 14:54:00 +0300

    Add support for Python 3.7

--- setup.py
+++ setup.py
@@ -94,6 +94,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Operating System :: OS Independent',
     'Topic :: Software Development :: Libraries',
     'Topic :: Software Development :: Libraries :: Python Modules',
